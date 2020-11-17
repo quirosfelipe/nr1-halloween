dc15a89349fea4ff57555142ed8a8e0595007bdc
Felipe Quiros <quirosfelipe@gmail.com>
2020-11-16 19:32:28 -0500

    catalog added

---
+++ nerdlets/felipe-nerdpack1-nerdlet/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
+
+export default class FelipeNerdpack1NerdletNerdlet extends React.Component {
+  render() {
+    return <h1>Is halloween over?</h1>;
+  }
+}
